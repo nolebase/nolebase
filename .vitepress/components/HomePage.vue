65ae01686600924df0080578934ed7e95f08cd9b
Ayaka Neko <neko@ayaka.moe>
2022-09-13 23:22:04 +0800

    feat: 添加首页，完善配置和搭建

---
+++ .vitepress/components/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { VPTeamMembers } from "vitepress/theme";
+import { creators } from '../creators'
+</script>
+
+<template>
+  <div class="content">
+    <div class="content-container">
+      <main class="main">
+        <div class="vp-doc" flex flex-col items-center mt-10>
+          <h2 id="meet-the-team" op50 font-normal p="t-10 b-2">
+            知识库的创作者
+          </h2>
+          <div w-full p-10>
+            <VPTeamMembers size="small" :members="creators" />
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,15 +1,57 @@
 import { defineConfigWithTheme } from 'vitepress'
 import { sidebar } from './metainfo.json'
+import {
+  siteName,
+  siteDescription,
+  githubRepoLink,
+} from './meta'
 
 export default defineConfigWithTheme({
-  title: 'notebook',
-  description: 'Just playing around.',
+  lang: 'zh-CN',
+  title: siteName,
+  description: siteDescription,
+  lastUpdated: true,
+  head: [
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
+    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'alternate icon', href: '/favicon.ico', type: 'image/png', sizes: '16x16' }],
+    ['meta', { name: 'author', content: `Ayaka Neko, Ayaka Rizumu` }],
+    ['meta', { name: 'keywords', content: 'markdown, knowledgebase, 知识库, vitepress, obsidian, notebook, notes, nekomeowww, littlesound' }],
+    ['meta', { property: 'og:title', content: siteName }],
+    ['meta', { property: 'og:description', content: siteDescription }],
+    ['meta', { name: 'twitter:title', content: siteName }],
+    ['meta', { name: 'twitter:description', content: siteDescription }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#927baf' }],
+    ['link', {rel: 'manifest', href: '/site.webmanifest' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#603cba' }],
+  ],
   themeConfig: {
     outline: 'deep',
+    editLink: {
+      pattern: 'https://github.com/LittleSound/notebook/tree/main/:path',
+      text: '编辑本页面',
+    },
+    localeLinks: {
+      text: '简体中文',
+    },
+    socialLinks: [
+      { icon: 'github', link: githubRepoLink },
+    ],
+    footer: {
+      message: '用 <span style="color: #e25555;">&#9829;</span> 撰写',
+      copyright: 'Copyright © 2022-PRESENT 絢香猫，绚香音',
+    },
     nav: [
-      { text: 'Home', link: '/' },
+      { text: '主页', link: '/' },
       { text: '笔记', link: '/笔记/index' },
     ],
     sidebar,
   },
+  markdown: {
+    theme: {
+      light: 'one-dark-pro',
+      dark: 'one-dark-pro',
+    }
+  }
 })
