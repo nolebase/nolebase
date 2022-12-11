eb8cb6326d3717d2bc1212f8ec9ddbd463a7ee78
Ayaka Neko <neko@ayaka.moe>
2022-12-11 17:55:37 +0800

    feat: 添加 functions 日志，修复 domain contraint 问题

--- netlify/functions/to.ts
+++ netlify/functions/to.ts
@@ -1,7 +1,7 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import fetch from "node-fetch";
 
-const TargetDomain = 'https://nolebase.ayaka.io';
+const TargetDomain = 'nolebase.ayaka.io';
 const FunctionEndpoint = "/.netlify/functions/to";
 const HyphenQueryShortURLEndpoint = 'https://api.ayaka.io/hyphen/api/v1/url/short';
 
@@ -21,11 +21,12 @@ const handler: Handler = async (event: HandlerEvent) => {
       .replace(/^\//, ""); // 去除开头的斜杠
 
     // 获取短链接对应的长链接
-    const res = await fetch(
-      `${HyphenQueryShortURLEndpoint}?url=${shortUrl}`
-    );
+    const requestUri = `${HyphenQueryShortURLEndpoint}?url=${shortUrl}`
+    console.log(`requestUri: ${requestUri}`)
+    const res = await fetch(requestUri);
     // 如果返回了 404 则跳转到 404 页面
     if (res.status === 404) {
+      console.warn(`404: ${shortUrl}`);
       return {
         statusCode: 301,
         headers: {
@@ -39,6 +40,7 @@ const handler: Handler = async (event: HandlerEvent) => {
       (await res.json()) as HyphenResponse<HyphenQueryShortURLResponse>;
     // 如果 data.url 为空则跳转到 404 页面
     if (resJson.data.url === "") {
+      console.warn(`url is empty: ${resJson.data.url}`);
       return {
         statusCode: 301,
         headers: {
@@ -48,9 +50,10 @@ const handler: Handler = async (event: HandlerEvent) => {
     }
 
     // 转换为 URL 对象
-    const url = new URL(TargetDomain)
+    const url = new URL(resJson.data.url)
     // 如果 host 不是目标域名则跳转到 404 页面
     if (url.host !== TargetDomain) {
+      console.warn(`target domain mismatch, expect: `, TargetDomain, 'got: ', url.host);
       return {
         statusCode: 301,
         headers: {
@@ -67,6 +70,7 @@ const handler: Handler = async (event: HandlerEvent) => {
       }
     };
   } catch (err) {
+    console.error(err);
     // 如果出现错误则返回 502
     return {
       statusCode: 502,
