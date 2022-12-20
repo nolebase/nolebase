e72f325b125a4621122c448da66d0fbe5c070c45
LittleSound <464388324@qq.com>
2022-12-20 23:15:19 +0800

    fix: 水合错误 3

--- .vitepress/theme/composables/route.ts
+++ .vitepress/theme/composables/route.ts
@@ -1,7 +1,12 @@
+import { useMounted } from '@vueuse/core'
 import { useRoute } from 'vitepress'
 import { computed } from 'vue'
 
 export function useRawPath() {
   const route = useRoute()
-  return computed(() => decodeURIComponent(route.path).replace(/^\/(.+)\.html$/, '$1.md').toLowerCase())
+  const isMounted = useMounted()
+  return computed(() => (isMounted
+    ? decodeURIComponent(route.path).replace(/^\/(.+)\.html$/, '$1.md').toLowerCase()
+    : ''
+  ))
 }
