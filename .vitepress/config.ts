45f41934396a3c61e79983ea5269eba602f8c575
LittleSound <464388324@qq.com>
2023-02-01 14:25:03 +0800

    fix: notes nav is not highlighting

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -54,7 +54,7 @@ export default defineConfigWithTheme({
     },
     nav: [
       { text: '主页', link: '/' },
-      { text: '笔记', link: '/笔记/index' },
+      { text: '笔记', link: '/笔记/' },
       { text: '最近更新', link: '/toc' },
     ],
     sidebar,
