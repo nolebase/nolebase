87d99c188d31ec82ac9783e76fbcc28e45bd9878
Ayaka Neko <neko@ayaka.moe>
2023-09-06 15:20:18 +0800

    chore: 修复 lint 和默认样式的问题

--- .vitepress/plugins/vitepress-plugin-docsmd-easytag/src/utils/reliability.ts
+++ .vitepress/plugins/vitepress-plugin-docsmd-easytag/src/utils/reliability.ts
@@ -8,17 +8,19 @@ import { sleep } from './time'
  * @param func 调用函数
  * @returns 返回一个闭包后的函数，该函数的签名与 func 一致
  */
-export function toRetriable<A extends any, R extends any>(retryLimit: number, delayInterval: number, func: (...args: A[]) => Promise<R>): (...args: A[]) => Promise<R> {
+export function toRetriable<A, R>(retryLimit: number, delayInterval: number, func: (...args: A[]) => Promise<R>): (...args: A[]) => Promise<R> {
   let retryCount = 0
   return async function (args: A): Promise<R> {
     try {
       return await func(args)
-    } catch (err) {
+    }
+    catch (err) {
       if (retryCount < retryLimit) {
         retryCount++
         await sleep(delayInterval)
         return await toRetriable(retryLimit, delayInterval, func)(args)
-      } else {
+      }
+      else {
         throw err
       }
     }
