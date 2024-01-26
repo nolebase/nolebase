516d8bcdab4534eef4e1c0a9481acaa4d8378d19
Neko Ayaka <neko@ayaka.moe>
2024-01-27 02:11:31 +0800

    feat: 迁移 easytags 到 @nolebase/vitepress-plugin-page-properties (#34)
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

--- .vitepress/plugins/vitepress-plugin-docsmd-easytag/src/utils/time.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 睡眠函数
- *
- * @param duration 睡眠时间，单位毫秒
- */
-export async function sleep(duration: number) {
-  return new Promise(resolve => setTimeout(resolve, duration))
-}
--- .vitepress/plugins/vitepress-plugin-docsmd-easytag/src/utils/verbose.ts
+++
@@ -1,5 +0,0 @@
-import { env } from 'node:process'
-
-export function isVerboseOn(): boolean {
-  return env.VERBOSE === 'true' || env.VERBOSE === '1' || env.VERBOSE === 'on'
-}
