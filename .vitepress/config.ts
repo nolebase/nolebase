22dbc057d5b671e4039f15e1ab45a1b11ba98c33
Rizumu Ayaka <rizumu@ayaka.moe>
2024-04-11 01:10:14 +0800

    feat(search): include tags

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -159,6 +159,49 @@ export default defineConfig({
             },
           },
         },
+
+        // Add title ang tags field in frontmatter to search
+        // You can exclude a page from search by adding search: false to the page's frontmatter.
+        _render(src, env, md) {
+          // without `md.render(src, env)`, the some information will be missing from the env.
+          let html = md.render(src, env)
+          let tagsPart = ''
+          let headingPart = ''
+          let contentPart = ''
+          let fullContent = ''
+          const sortContent = () => [headingPart, tagsPart, contentPart] as const
+          let { frontmatter, content } = env
+
+          if (!frontmatter)
+            return html
+
+          if (frontmatter.search === false)
+            return ''
+
+          contentPart = content ||= src
+
+          const headingMatch = content.match(/^#{1} .*/m)
+          const hasHeading = !!(headingMatch && headingMatch[0] && headingMatch.index !== undefined)
+
+          if (hasHeading) {
+            const headingEnd = headingMatch.index! + headingMatch[0].length
+            headingPart = content.slice(0, headingEnd)
+            contentPart = content.slice(headingEnd)
+          }
+          else if (frontmatter.title) {
+            headingPart = `# ${frontmatter.title}`
+          }
+
+          const tags = frontmatter.tags
+          if (tags && Array.isArray(tags) && tags.length)
+            tagsPart = `Tags: #${tags.join(', #')}`
+
+          fullContent = sortContent().filter(Boolean).join('\n\n')
+
+          html = md.render(fullContent, env)
+
+          return html
+        },
       },
     },
     nav: [
